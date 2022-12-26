f6753a303125d79245b20df38d96d3cf68724e81
Oneted11 <muriithited@gmail.com>
2022-12-26 11:20:35 +0300

    feat:setup UI

---
+++ readme.md
@@ -0,0 +1,13 @@
+## game flow
+- needs to track any clicks that happen on our cells
+- needs to check if a valid move has been made
+- needs to make sure nothing happens if an already played cell has been clicked
+- we should update our game state
+- we should validate the game state
+- check if a player has won
+- check if the game ended in a draw
+- either stop the game or change the active player, depending on the above checks
+- reflect the updates made on the UI
+- rinse and repeat
+
+# Learnings
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [game, setGame] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X");
+  const [active, setActive] = useState();
+  const [status, setStatus] = useState(`It's ${player}'s turn`);
+
+  function handleCellPlayed() {}
+  function handlePlayerChange() {}
+  function handleResultValidation() {}
+  function handleCellClick() {}
+  function handleRestartGame() {}
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="game--title">Tic Tac Toe</h1>
+      <div className="game--container">
+        <div data-cell-index="0" className="cell"></div>
+        <div data-cell-index="1" className="cell"></div>
+        <div data-cell-index="2" className="cell"></div>
+        <div data-cell-index="3" className="cell"></div>
+        <div data-cell-index="4" className="cell"></div>
+        <div data-cell-index="5" className="cell"></div>
+        <div data-cell-index="6" className="cell"></div>
+        <div data-cell-index="7" className="cell"></div>
+        <div data-cell-index="8" className="cell"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2 className="game--status">{status}</h2>
+      <button className="game--restart">Restart Game</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
